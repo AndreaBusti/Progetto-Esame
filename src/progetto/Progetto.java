ddb21e3000fa4f675bef434cc7633a63735b1ecd
AndreaBusti <andreabusti19@gmail.com>
2021-04-20 11:33:47 +0100

    modifica

--- src/progetto/Progetto.java
+++ src/progetto/Progetto.java
@@ -17,6 +17,7 @@ public class Progetto {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("sono bello");
+        System.out.println("renato");
     }
     
 }
